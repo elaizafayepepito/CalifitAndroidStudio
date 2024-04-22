0f0a3f3d6863c90f9b2932ae65fb05a69ff2690f
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-04-22 10:13:00 +0800

    Fix welcomepage

--- califit/app/src/main/java/com/example/califit/MainActivity.kt
+++ califit/app/src/main/java/com/example/califit/MainActivity.kt
@@ -7,13 +7,15 @@ import androidx.fragment.app.commit
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        showSplashScreen()
+        setContentView(R.layout.activity_main)
+        showSplashScreen() // Call this function to show the splash screen
     }
 
     private fun showSplashScreen() {
+        // Use supportFragmentManager to begin a transaction, replace a container in your layout (here using android.R.id.content as the container) with your SplashScreenFragment
         supportFragmentManager.commit {
-            replace(android.R.id.content, SplashScreenFragment())
+            setReorderingAllowed(true) // Optimize the fragment transaction
+            replace(android.R.id.content, SplashScreenFragment()) // Replace the entire activity content with the splash screen
         }
     }
 }
-
