ca9845824ddd3d03b540d4febd0e423fbd29820a
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-04-23 23:47:31 +0800

    create exercise selection view

---
+++ califit/app/src/main/java/com/example/califit/ExerciseSelection.kt
@@ -0,0 +1,24 @@
+package com.example.califit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ExerciseSelection : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise)
+
+        val exerciseCard1 = findViewById<ExerciseCardView>(R.id.exerciseCard1)
+        exerciseCard1.setCardTitle("Tabletop Crunch")
+        exerciseCard1.setImageResource(R.drawable.tabletop_crunch)
+
+        val exerciseCard2 = findViewById<ExerciseCardView>(R.id.exerciseCard2)
+        exerciseCard2.setCardTitle("Inclined Pushup")
+        exerciseCard2.setImageResource(R.drawable.inclined_pushup)
+
+        val exerciseCard2 = findViewById<ExerciseCardView>(R.id.exerciseCard2)
+        exerciseCard2.setCardTitle("Inclined Pushup")
+        exerciseCard2.setImageResource(R.drawable.sumo_squat)
+    }
+}
