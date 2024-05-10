f9381d2de02b3ba2ec8cb0dae86b7fe358720a5d
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-05-10 14:32:04 +0800

    Create Login

---
+++ califit/app/src/main/java/com/example/califit/LoginActivity.java
@@ -0,0 +1,67 @@
+package com.example.califit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_login);
+
+        Button buttonSignIn = findViewById(R.id.buttonSignIn);
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToExerciseSelection();
+            }
+        });
+
+        // Add the TextView for signing up
+        TextView textViewSignup = findViewById(R.id.textViewSignup);
+        makePartOfTextClickable(textViewSignup, "Don't have an account? ", "Signup");
+    }
+
+    private void makePartOfTextClickable(TextView textView, String nonLinkText, String linkText) {
+        SpannableString spannableString = new SpannableString(nonLinkText + linkText);
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                navigateToUserRegistration();
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(true);  // Set to false if you don't want underline
+                ds.setColor(getResources().getColor(R.color.white)); // Define this color in your colors.xml
+            }
+        };
+
+        spannableString.setSpan(clickableSpan, nonLinkText.length(), (nonLinkText + linkText).length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.setText(spannableString);
+        textView.setMovementMethod(LinkMovementMethod.getInstance()); // This makes the link clickable
+    }
+
+    public void navigateToUserRegistration() {
+        Intent intent = new Intent(this, UserRegistrationFragment.class);
+        startActivity(intent);
+    }
+
+    private void navigateToExerciseSelection() {
+        Intent intent = new Intent(this, ExerciseSelectionActivity.class);
+        startActivity(intent);
+    }
+}
