7e1ed1bf098465901a59c26923a0aed731ba70c6
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-05-10 14:29:51 +0800

    Create Age Registration

--- califit/app/src/main/java/com/example/califit/AgeRegistrationFragment.java
+++ califit/app/src/main/java/com/example/califit/AgeRegistrationFragment.java
@@ -1,5 +1,6 @@
 package com.example.califit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,21 +8,23 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class AgeRegistrationFragment extends Fragment {
+public class AgeRegistrationFragment extends AppCompatActivity {
 
     private EditText editTextAge;
     private Button buttonNextAge;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_age_registration, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_age_registration);
 
-        editTextAge = view.findViewById(R.id.editTextAge);
-        buttonNextAge = view.findViewById(R.id.buttonNextAge);
+        Button buttonNextAge = findViewById(R.id.buttonNextAge);
 
         buttonNextAge.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,29 +32,10 @@ public class AgeRegistrationFragment extends Fragment {
                 navigateToGenderRegistration();
             }
         });
-
-        return view;
     }
 
     private void navigateToGenderRegistration() {
-        String ageInputString = editTextAge.getText().toString();
-        Integer ageInput = null;
-        try {
-            ageInput = Integer.parseInt(ageInputString);
-        } catch (NumberFormatException e) {
-            Toast.makeText(getContext(), "Please enter a valid age.", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        if (ageInput != null && ageInput > 0) {
-            GenderRegistrationFragment genderRegistrationFragment = new GenderRegistrationFragment();
-            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, genderRegistrationFragment);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
-        } else {
-            Toast.makeText(getContext(), "Please enter a valid age.", Toast.LENGTH_SHORT).show();
-        }
+        Intent intent = new Intent(this, GenderRegistrationFragment.class);
+        startActivity(intent);
     }
 }
