ccb2e47e64376838beb560d13926cae9b9832150
Ginalyn Caneda <ginalyn.caneda@cit.edu>
2024-05-15 18:58:37 +0800

    Add gender selection handler and create user functionality

--- califit/app/src/main/java/com/example/califit/GenderRegistrationFragment.java
+++ califit/app/src/main/java/com/example/califit/GenderRegistrationFragment.java
@@ -5,8 +5,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class GenderRegistrationFragment extends AppCompatActivity {
 
@@ -15,6 +22,10 @@ public class GenderRegistrationFragment extends AppCompatActivity {
     private String lastname;
     private String age;
     private Button buttonFinish;
+    private String gender;
+    private Button buttonMale;
+    private Button buttonFemale;
+    private UserAccountService userAccountService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +42,84 @@ public class GenderRegistrationFragment extends AppCompatActivity {
         Log.d("GenderRegistration", "Lastname received: " + lastname);
         Log.d("GenderRegistration", "Age received: " + age);
 
+        buttonMale = findViewById(R.id.buttonMale);
+        buttonFemale = findViewById(R.id.buttonFemale);
         buttonFinish = findViewById(R.id.buttonFinish);
+
+        userAccountService = new UserAccountService(this);
+
+        buttonMale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectGender("Male");
+            }
+        });
+
+        buttonFemale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectGender("Female");
+            }
+        });
 
         buttonFinish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                navigateToDashboard();
+                createUser();
             }
         });
+    }
+
+    private void selectGender(String selectedGender) {
+        if (selectedGender.equals("Male")) {
+            gender = "Male";
+            buttonMale.setEnabled(false);
+            buttonFemale.setEnabled(true);
+        } else if (selectedGender.equals("Female")) {
+            gender = "Female";
+            buttonMale.setEnabled(true);
+            buttonFemale.setEnabled(false);
+        }
+        Log.d("GenderRegistration", "Selected gender: " + gender);
+    }
+
+    private void createUser() {
+        try {
+            JSONObject userData = new JSONObject();
+            userData.put("accountId", accountId);
+            userData.put("firstname", firstname);
+            userData.put("lastname", lastname);
+            userData.put("age", Integer.parseInt(age));
+            userData.put("gender", gender);
+
+            userAccountService.createUser(userData, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        String message = response.getString("message");
+                        if (message.equals("User created successfully")) {
+                            Toast.makeText(GenderRegistrationFragment.this, message, Toast.LENGTH_LONG).show();
+                            navigateToDashboard();
+                        } else {
+                            Toast.makeText(GenderRegistrationFragment.this, message, Toast.LENGTH_LONG).show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(GenderRegistrationFragment.this, "Error parsing response", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                    Toast.makeText(GenderRegistrationFragment.this, "Error creating user: " + error.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Error creating user data", Toast.LENGTH_LONG).show();
+        }
     }
 
     private void navigateToDashboard() {
