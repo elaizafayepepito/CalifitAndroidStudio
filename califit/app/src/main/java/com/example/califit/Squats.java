2fb531be5593e1aadba9f3aa97314fe99ec9dbb3
Elaiza Faye Pepito <elaizafaye.pepito@cit.edu>
2024-05-18 12:08:14 +0800

    Create classes for all exercises

---
+++ califit/app/src/main/java/com/example/califit/Squats.java
@@ -0,0 +1,45 @@
+package com.example.califit;
+
+public class Squats {
+    private String userId;
+    private int reps;
+    private String date;
+    private String timeStarted;
+    private String timeEnded;
+    private double averageAngleDepth;
+
+    public Squats() {}
+
+    public Squats(String userId, int reps, String date, String timeStarted, String timeEnded, double averageAngleDepth) {
+        this.userId = userId;
+        this.reps = reps;
+        this.date = date;
+        this.timeStarted = timeStarted;
+        this.timeEnded = timeEnded;
+        this.averageAngleDepth = averageAngleDepth;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTimeStarted() {
+        return timeStarted;
+    }
+
+    public String getTimeEnded() {
+        return timeEnded;
+    }
+
+    public double getAverageAngleDepth() {
+        return averageAngleDepth;
+    }
+}
